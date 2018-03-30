446b6d0774d6d8f9dbdf919f542c2ffe9b30b37f
Téo Martin <37612909+teo-martin@users.noreply.github.com>
2018-03-30 17:05:28 +0200

    Add files via upload
    ajout exercices 1 à 3

---
+++ Exo2/DureeRelOps/TestCDuree.cpp
@@ -0,0 +1,73 @@
+/**
+ *
+ * \file     TestDuree.cpp
+ *
+ * \authors  M. Laporte, D. Mathieu
+ *
+ * \date     29/10/2007
+ *
+ * \version  V1.0
+ *
+ * \brief    Test des operateurs + et -
+ *
+ **/
+#include <iostream>
+#include <iomanip>            // setw()
+#include <vector>
+#include <utility>
+
+using namespace std;
+using namespace rel_ops;
+
+#include "duree.h"
+
+using namespace nsUtil;
+
+#define classdef typedef
+
+namespace 
+{
+    void testDuree (void)
+    {
+        Duree d1 (123);
+        Duree d2 (12);
+
+        d1.display ();
+        cout << " + ";
+        d2.display ();
+        cout << " = ";
+        (d1 + d2).display ();
+        cout << '\n';
+
+        d2.display ();
+        cout << " + ";
+        d1.display ();
+        cout << " = ";
+        (d2 + d1).display ();
+        cout << '\n';
+
+        d1.display ();
+        cout << " - ";
+        d2.display ();
+        cout << " = ";
+        (d1 - d2).display ();
+        cout << '\n';
+
+        d2.display ();
+        cout << " - ";
+        d1.display ();
+        cout << " = ";
+        (d2 - d1).display ();
+        cout << '\n';
+
+    } // TestCDuree()
+
+} // namespace anonyme
+
+/*int main (void)
+{
+    testDuree ();
+
+    return 0;
+
+} // main() */
---
+++ Exo2/DureeRelOps/duree.h
@@ -0,0 +1,31 @@
+namespace nsUtil
+{
+    typedef unsigned long long ULL_t;
+
+    class Duree
+    {
+    public:
+        Duree(ULL_t sec = 0);
+        Duree(const Duree & duree);
+        void display();
+        ULL_t getDuree() const;
+        void incr(const ULL_t delta = 0);
+        void decr(const ULL_t delta = 0);
+        Duree operator + (const Duree & duree);
+        Duree operator - (const Duree & duree);
+        bool operator < (const Duree & duree) const;
+        bool operator != (const Duree & duree) const;
+        bool DureeSortSearchoperator == (const Duree & duree) const;
+        ~Duree(void);
+
+    private:
+        ULL_t myDuree;
+        unsigned short mySeconds;
+        unsigned short myMinutes;
+        unsigned short myHours;
+        unsigned short myDays;
+
+        void normaliser();
+    }; //class Duree
+}
+
